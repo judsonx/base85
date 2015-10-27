d385360cda2fca93b610c08abbe5caafa24074a2
Judson Weissert <judson.weissert@gmail.com>
2015-10-26 23:53:10 -0400

    Significant test refactoring.

--- src/test.c
+++ src/test.c
@@ -1,5 +1,6 @@
 #include "base85/base85.h"
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -20,37 +21,6 @@ struct b85_test_t
 
 static const char zeros[32];
 static const char binary1[] = { 0xff, 0xd8, 0xff, 0xe0 };
-
-static const struct b85_test_t g_small_tests[] = {
-  { { "h", 1 }, { "BE", 2 } },
-  { { "he", 2 }, { "BOq", 3 } },
-  { { "hel", 3 }, { "BOtu", 4 } },
-  { { "hell", 4 }, { "BOu!r", 5 } },
-  { { "hello", 5 }, { "BOu!rDZ", 7 } },
-  { { "hello ", 6 }, { "BOu!rD]f", 8 } },
-  { { "hello w", 7 }, { "BOu!rD]j6", 9 } },
-  { { "hello wo", 8 }, { "BOu!rD]j7B", 10 } },
-  { { "hello wor", 9 }, { "BOu!rD]j7BEW", 12 } },
-  { { "hello worl", 10 }, { "BOu!rD]j7BEbk", 13 } },
-  { { "hello world", 11 }, { "BOu!rD]j7BEbo7", 14 } },
-  { { "hello world!", 12 }, { "BOu!rD]j7BEbo80", 15 } },
-
-  { { zeros, 4 }, { "z", 1 } },
-  { { zeros, 8 }, { "zz", 2 } },
-  { { zeros, 12 }, { "zzz", 3 } },
-  { { zeros, 16 }, { "zzzz", 4 } },
-  { { zeros, 20 }, { "zzzzz", 5 } },
-  { { zeros, 24 }, { "zzzzzz", 6 } },
-  { { zeros, 28 }, { "zzzzzzz", 7 } },
-  { { zeros, 32 }, { "zzzzzzzz", 8 } },
-
-  { { binary1, 4 }, { "s4IA0", 5 }}
-};
-
-static const struct b85_test_t g_whitespace_tests[] = {
-  { { "hello world", 11 }, { " B\tOu  !rD]\nj7B\rEb o7  ", 23 } },
-  { { "hello world", 11 }, { "B\t\n\nOu!  rD]j7BEbo7\r\n", 21 } },
-};
 
 int
 run_ws_test (const struct b85_test_t *entry)
@@ -133,60 +103,90 @@ run_small_test (const struct b85_test_t *entry)
   return -1;
 }
 
-int
-run_small_tests ()
-{
-  printf ("small tests: ");
-  size_t count = 0;
-  for (size_t i = 0; i < dimof (g_small_tests); ++i)
-  {
-    if (run_small_test (&g_small_tests[i]))
-      break;
-
-    ++count;
-  }
-
-  if (dimof (g_small_tests) != count)
-  {
-    printf ("FAIL [%zu]\n", count);
-    return -1;
-  }
-
-  printf ("PASS [%zu]\n", count);
-  return 0;
+#define B85_CREATE_TEST(name, test, input, input_cb, encoded, encoded_cb) \
+bool b85_test_##name () { \
+  struct b85_test_t data = { { input, input_cb }, { encoded, encoded_cb } }; \
+  return !test (&data); \
 }
 
-int
-run_ws_tests ()
-{
-  printf ("decode whitespace: ");
-  size_t count = 0;
-  for (size_t i = 0; i < dimof (g_whitespace_tests); ++i)
-  {
-    if (run_ws_test (&g_whitespace_tests[i]))
-      break;
+#define B85_RUN_TEST(name) do { \
+  if (b85_test_##name()) \
+  { \
+    ++count; \
+    printf ("  PASS :: %s\n", #name); \
+  } \
+  else \
+  { \
+    printf ("  FAIL :: %s\n", #name); \
+  } \
+  ++total; \
+} while (0);
 
-    ++count;
-  }
 
-  if (dimof (g_whitespace_tests) != count)
-  {
-    printf ("FAIL [%zu]\n", count);
-    return -1;
-  }
+B85_CREATE_TEST (s1, run_small_test, "h", 1, "BE", 2)
+B85_CREATE_TEST (s2, run_small_test, "he", 2, "BOq", 3)
+B85_CREATE_TEST (s3, run_small_test, "hel", 3, "BOtu", 4)
+B85_CREATE_TEST (s4, run_small_test, "hell", 4, "BOu!r", 5)
+B85_CREATE_TEST (s5, run_small_test, "hello", 5, "BOu!rDZ", 7)
+B85_CREATE_TEST (s6, run_small_test, "hello ", 6, "BOu!rD]f", 8)
+B85_CREATE_TEST (s7, run_small_test, "hello w", 7, "BOu!rD]j6", 9)
+B85_CREATE_TEST (s8, run_small_test, "hello wo", 8, "BOu!rD]j7B", 10)
+B85_CREATE_TEST (s9, run_small_test, "hello wor", 9, "BOu!rD]j7BEW", 12)
+B85_CREATE_TEST (s10, run_small_test, "hello worl", 10, "BOu!rD]j7BEbk", 13)
+B85_CREATE_TEST (s11, run_small_test, "hello world", 11, "BOu!rD]j7BEbo7", 14)
+B85_CREATE_TEST (s12, run_small_test, "hello world!", 12, "BOu!rD]j7BEbo80", 15)
 
-  printf ("PASS [%zu]\n", count);
-  return 0;
-}
+B85_CREATE_TEST (z1, run_small_test, zeros, 4, "z", 1)
+B85_CREATE_TEST (z2, run_small_test, zeros, 8, "zz", 2)
+B85_CREATE_TEST (z3, run_small_test, zeros, 12, "zzz", 3)
+B85_CREATE_TEST (z4, run_small_test, zeros, 16, "zzzz", 4)
+B85_CREATE_TEST (z5, run_small_test, zeros, 20, "zzzzz", 5)
+B85_CREATE_TEST (z6, run_small_test, zeros, 24, "zzzzzz", 6)
+B85_CREATE_TEST (z7, run_small_test, zeros, 28, "zzzzzzz", 7)
+B85_CREATE_TEST (z8, run_small_test, zeros, 32, "zzzzzzzz", 8)
+
+B85_CREATE_TEST (bin1, run_small_test, binary1, 4,"s4IA0", 5)
+
+B85_CREATE_TEST (ws1, run_ws_test, "hello world", 11, " B\tOu  !rD]\nj7B\rEb o7  ", 23)
+B85_CREATE_TEST (ws2, run_ws_test, "hello world", 11, "B\t\n\nOu!  rD]j7BEbo7\r\n", 21)
 
 int
 run_tests (int argc, char *argv[])
 {
-  if (run_small_tests ())
-    return 1;
-  if (run_ws_tests ())
-    return 1;
+  size_t count = 0;
+  size_t total = 0;
 
+  printf ("small tests:\n");
+  B85_RUN_TEST (s1);
+  B85_RUN_TEST (s2);
+  B85_RUN_TEST (s3);
+  B85_RUN_TEST (s4);
+  B85_RUN_TEST (s5);
+  B85_RUN_TEST (s6);
+  B85_RUN_TEST (s7);
+  B85_RUN_TEST (s8);
+  B85_RUN_TEST (s9);
+  B85_RUN_TEST (s10);
+  B85_RUN_TEST (s11);
+  B85_RUN_TEST (s12);
+  B85_RUN_TEST (z1);
+  B85_RUN_TEST (z2);
+  B85_RUN_TEST (z3);
+  B85_RUN_TEST (z4);
+  B85_RUN_TEST (z5);
+  B85_RUN_TEST (z6);
+  B85_RUN_TEST (z7);
+  B85_RUN_TEST (z8);
+  B85_RUN_TEST (bin1);
+
+  printf ("decode whitespace:\n");
+  B85_RUN_TEST (ws1);
+  B85_RUN_TEST (ws2);
+
+  printf ("\nTOTAL: %zu FAILED: %zu\n", total, total - count);
+
+  if (total != count)
+    return 1;
   return 0;
 }
 
