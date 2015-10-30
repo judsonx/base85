195a401a730ba967efbdd6972be3601d47d4fb12
Judson Weissert <judson.weissert@gmail.com>
2015-10-29 21:48:23 -0400

    Add support for encoding/decoding to/from files.
    TODO: Improve read/write error handling.

--- README.md
+++ README.md
@@ -23,9 +23,22 @@ Currently, tests only exist for the 'ascii85' encoder/decoder.
 
 ## CLI
 
-The command line applications, `ascii85`, and `z85` read from `stdin` and write
-to `stdout`. Run `ascii85 -e` to encode data from `stdin`, and run `ascii85 -d`
-to decode data from `stdin` (same holds for `z85`).
+The command line utilities `ascii85`` and `z85` can be used to encode and
+decode data from **stdin**/**stdout** or from files.
+
+Read from **stdin** and write to **stdout**:
+  - Encode: `ascii85 -e`
+  - Decode: `ascii85 -d`
+
+Read from a file and write to **stdout**:
+  - Encode: `ascii85 -e source`
+  - Decode: `ascii85 -d source`
+
+Read from a file and write to another file:
+  - Encode: `ascii85 -e source destination`
+  - Decode: `ascii85 -d source destination`
+
+The same arguments are supported by the `z85` command.
 
 ### License
 
--- src/main.c
+++ src/main.c
@@ -11,12 +11,13 @@ static const size_t INPUT_BUFFER_MAX = 1024;
 static int
 usage (const char *name)
 {
-  fprintf (stderr, "Usage: %s -e | -d\n", name);
+  fprintf (stderr, "Usage: %s -e | -d [input_file [output_file]]\n", name);
   return 2;
 }
 
 static size_t
-print_max_width (const char *buf, size_t buf_cb, size_t width, size_t offset)
+print_max_width (
+  FILE *fh, const char *buf, size_t buf_cb, size_t width, size_t offset )
 {
   // How many bytes were written to the last row.
   size_t rv = (buf_cb + offset) % width;
@@ -27,9 +28,9 @@ print_max_width (const char *buf, size_t buf_cb, size_t width, size_t offset)
     if (buf_cb < cb)
       cb = buf_cb;
 
-    (void) write (1, buf, cb);
+    (void) fwrite (buf, 1, cb, fh);
     if (cb == width - offset)
-      (void) putchar ('\n');
+      (void) fwrite ("\n", 1, 1, fh);
 
     buf_cb -= cb;
     buf += cb;
@@ -39,7 +40,7 @@ print_max_width (const char *buf, size_t buf_cb, size_t width, size_t offset)
 }
 
 static b85_result_t
-b85_encode (struct base85_context_t *ctx)
+b85_encode (struct base85_context_t *ctx, FILE *fh_in, FILE *fh_out)
 {
   char input[INPUT_BUFFER_MAX];
   b85_result_t rv = B85_E_UNSPECIFIED;
@@ -48,7 +49,7 @@ b85_encode (struct base85_context_t *ctx)
   size_t cb = 0;
   size_t input_cb;
   char *out = NULL;
-  while ((input_cb = read (0, input, INPUT_BUFFER_MAX)))
+  while ((input_cb = fread (input, 1, INPUT_BUFFER_MAX, fh_in)))
   {
     rv = B85_ENCODE (input, input_cb, ctx);
     if (rv)
@@ -56,7 +57,7 @@ b85_encode (struct base85_context_t *ctx)
 
     out = B85_GET_OUTPUT (ctx, &cb);
     print_offset = print_max_width (
-      out, cb, ENCODED_LINE_LENGTH, print_offset
+      fh_out, out, cb, ENCODED_LINE_LENGTH, print_offset
     );
     B85_CLEAR_OUTPUT (ctx);
   }
@@ -65,21 +66,21 @@ b85_encode (struct base85_context_t *ctx)
     return rv;
 
   out = B85_GET_OUTPUT (ctx, &cb);
-  (void) print_max_width (out, cb, ENCODED_LINE_LENGTH, print_offset);
-  (void) putchar ('\n');
+  (void) print_max_width (fh_out, out, cb, ENCODED_LINE_LENGTH, print_offset);
+  (void) fwrite ("\n", 1, 1, fh_out);
 
   return B85_E_OK;
 }
 
 static b85_result_t
-b85_decode (struct base85_context_t *ctx)
+b85_decode (struct base85_context_t *ctx, FILE *fh_in, FILE *fh_out)
 {
   char input[INPUT_BUFFER_MAX];
   b85_result_t rv = B85_E_UNSPECIFIED;
 
   size_t input_cb;
   char *out = NULL;
-  while ((input_cb = read (0, input, INPUT_BUFFER_MAX)))
+  while ((input_cb = fread (input, 1, INPUT_BUFFER_MAX, fh_in)))
   {
     rv = B85_DECODE (input, input_cb, ctx);
     if (rv)
@@ -87,7 +88,7 @@ b85_decode (struct base85_context_t *ctx)
 
     size_t out_cb;
     out = B85_GET_OUTPUT (ctx, &out_cb);
-    (void) write (1, out, out_cb);
+    (void) fwrite (out, 1, out_cb, fh_out);
     B85_CLEAR_OUTPUT (ctx);
   }
   rv = B85_DECODE_LAST (ctx);
@@ -96,20 +97,20 @@ b85_decode (struct base85_context_t *ctx)
 
   size_t out_cb;
   out = B85_GET_OUTPUT (ctx, &out_cb);
-  (void) write (1, out, out_cb);
+  (void) fwrite (out, 1, out_cb, fh_out);
 
   return B85_E_OK;
 }
 
-typedef b85_result_t (*handler_t) (struct base85_context_t *);
+typedef b85_result_t (*handler_t) (struct base85_context_t *, FILE *, FILE *);
 
 static b85_result_t
-b85_wrapper (handler_t handler)
+b85_wrapper (handler_t handler, FILE *fh_in, FILE *fh_out)
 {
   struct base85_context_t ctx;
   b85_result_t rv = B85_CONTEXT_INIT (&ctx);
   if (B85_E_OK == rv)
-    rv = handler (&ctx);
+    rv = handler (&ctx, fh_in, fh_out);
   if (rv)
   {
     fprintf (
@@ -119,19 +120,84 @@ b85_wrapper (handler_t handler)
     );
   }
   B85_CONTEXT_DESTROY (&ctx);
-  return (B85_E_OK == rv) ? 0 : 1;
+  return rv;
+}
+
+int
+open_file_handles (int argc, char *argv[], FILE **fh_in, FILE **fh_out)
+{
+  FILE *in = NULL;
+  FILE *out = NULL;
+
+  switch (argc)
+  {
+  case 4:
+    out = fopen (argv[3], "wb");
+    if (!out)
+    {
+      perror ("* Output fopen() error");
+      return 1;
+    }
+  case 3:
+    in = fopen (argv[2], "rb");
+    if (!in)
+    {
+      perror ("* Input fopen() error");
+      (void) fclose (out);
+      return 1;
+    }
+  case 2:
+    break;
+  default:
+    return usage (argv[0]);
+  }
+
+  if (in)
+    *fh_in = in;
+  if (out)
+    *fh_out = out;
+  return 0;
+}
+
+void
+close_file_handles (int argc, FILE **fh_in, FILE **fh_out)
+{
+  switch (argc)
+  {
+  case 4:
+    (void) fclose (*fh_out);
+  case 3:
+    (void) fclose (*fh_in);
+  }
+
+  *fh_in = NULL;
+  *fh_out = NULL;
 }
 
 int
 main (int argc, char *argv[])
 {
-  if (2 != argc)
+  FILE *fh_in = stdin;
+  FILE *fh_out = stdout;
+
+  b85_result_t rv = B85_E_UNSPECIFIED;
+  if (!strcmp (argv[1], "-e"))
+  {
+    int result = open_file_handles (argc, argv, &fh_in, &fh_out);
+    if (result)
+      return result;
+    rv = b85_wrapper (b85_encode, fh_in, fh_out);
+  }
+  else if (!strcmp (argv[1], "-d"))
+  {
+    int result = open_file_handles (argc, argv, &fh_in, &fh_out);
+    if (result)
+      return result;
+    rv = b85_wrapper (b85_decode, fh_in, fh_out);
+  }
+  else
     return usage (argv[0]);
 
-  if (!strcmp (argv[1], "-e"))
-    return b85_wrapper (b85_encode);
-  else if (!strcmp (argv[1], "-d"))
-    return b85_wrapper (b85_decode);
-
-  return usage (argv[0]);
+  close_file_handles (argc, &fh_in, &fh_out);
+  return rv;
 }
